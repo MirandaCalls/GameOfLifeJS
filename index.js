e887e9f847d4ecddda9d48b35bd78381f0a22c3d
SecondSerenity <gmaidenmueller@gmail.com>
2019-12-15 22:33:05 -0600

    Bottom padding

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {roundDown} from './lib.js';
 
 let content_div = document.getElementsByClassName("content")[0];
 let max_width = roundDown(content_div.clientWidth, 20) - 20;
-let max_height = roundDown(content_div.clientHeight, 20) - 10;
+let max_height = roundDown(content_div.clientHeight, 20) - 30;
 
 let board_canvas = document.getElementById("GameBoard");
 board_canvas.width = max_width;
